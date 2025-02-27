f23635dae0937484a2aca7de54e007efdca0480c
akhilss84 <72548712+akhilss84@users.noreply.github.com>
2025-02-27 13:01:54 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "6.23.0"
+    }
+  }
+}
+provider "google" {
+  project     = "even-environs-303912"
+  region      = "us-central1"
+}
